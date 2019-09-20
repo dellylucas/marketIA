789adb506cded314e856ffdd5036a9786ae5f57a
David Felipe Garcia Porras <davidfgarcia2709@hotmail.com>
2019-09-19 19:45:39 -0500

    tabla usuarios

--- storeIAFront-master/tables.php
+++ storeIAFront-master/tables.php
@@ -278,102 +278,32 @@ if($RespuestaApiData == "insert success!")
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                   <thead>
 
-<?php
-                  $data = file_get_contents("http://52.229.9.122:8085/v1/user");
-                  $products = json_decode($data, true);
- 
-                  foreach ($products as $product) {
-                  echo '<pre>';
-                  print_r($product);
-                  echo '</pre>';
-}
-?>
+
                     <tr>
                       <th>Cedula</th>
                       <th>Nombre</th>
                       <th>Apellido</th>
-                      <th></th>
-                      <th>Start date</th>
-                      <th>Salary</th>
+                      <th>Celular</th>
+                      <th>Correo</th>
+                      <th>Documento</th>
                     </tr>
-                  </thead>
-                  <tfoot>
+                    <?php
+                  $data = file_get_contents("http://52.229.9.122:8085/v1/user");
+                  $products = json_decode($data, true);
+ 
+                  foreach ($products as $product) {?>
+                  
+                  
                     <tr>
-                      <th>Name</th>
-                      <th>Position</th>
-                      <th>Office</th>
-                      <th>Age</th>
-                      <th>Start date</th>
-                      <th>Salary</th>
+                      <th><?php print_r($product['id']);  ?></th>
+                      <th><?php print_r($product['nombre']);  ?></th>
+                      <th><?php print_r($product['apellido']); ?></th>
+                      <th><?php print_r($product['celular']);  ?></th>
+                      <th><?php print_r($product['correo']); ?></th>
+                      <th><?php print_r($product['documento']); } ?></th>
                     </tr>
                   </tfoot>
-                  <tbody>
-                    
-                    <tr>
-                      <td>Cedric Kelly</td>
-                      <td>Senior Javascript Developer</td>
-                      <td>Edinburgh</td>
-                      <td>22</td>
-                      <td>2012/03/29</td>
-                      <td>$433,060</td>
-                    </tr>
-                    <tr>
-                      <td>Airi Satou</td>
-                      <td>Accountant</td>
-                      <td>Tokyo</td>
-                      <td>33</td>
-                      <td>2008/11/28</td>
-                      <td>$162,700</td>
-                    </tr>
-                    <tr>
-                      <td>Brielle Williamson</td>
-                      <td>Integration Specialist</td>
-                      <td>New York</td>
-                      <td>61</td>
-                      <td>2012/12/02</td>
-                      <td>$372,000</td>
-                    </tr>
-                    <tr>
-                      <td>Herrod Chandler</td>
-                      <td>Sales Assistant</td>
-                      <td>San Francisco</td>
-                      <td>59</td>
-                      <td>2012/08/06</td>
-                      <td>$137,500</td>
-                    </tr>
-                    <tr>
-                      <td>Sonya Frost</td>
-                      <td>Software Engineer</td>
-                      <td>Edinburgh</td>
-                      <td>23</td>
-                      <td>2008/12/13</td>
-                      <td>$103,600</td>
-                    </tr>
-                    <tr>
-                      <td>Jena Gaines</td>
-                      <td>Office Manager</td>
-                      <td>London</td>
-                      <td>30</td>
-                      <td>2008/12/19</td>
-                      <td>$90,560</td>
-                    </tr>
-                    <tr>
-                      <td>Quinn Flynn</td>
-                      <td>Support Lead</td>
-                      <td>Edinburgh</td>
-                      <td>22</td>
-                      <td>2013/03/03</td>
-                      <td>$342,000</td>
-                    </tr>
-                    <tr>
-                      <td>Charde Marshall</td>
-                      <td>Regional Director</td>
-                      <td>San Francisco</td>
-                      <td>36</td>
-                      <td>2008/10/16</td>
-                      <td>$470,600</td>
-                    </tr>
-                  </tbody>
+                  
                 </table>
               </div>
             </div>
