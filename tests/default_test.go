a2bfa46264f2f98968f85b4c9784968259af16e0
dellylucas <dellylucas@hotmail.com>
2019-11-26 16:44:34 -0500

    test units

--- tests/default_test.go
+++ tests/default_test.go
@@ -22,16 +22,18 @@ func init() {
 // TestGetAll obtiene todos los usuarios //go test tests/default_test.go -v
 func TestGetAll(t *testing.T) {
 	ob := models.GetAllUser()
-	beego.Trace("testing", "TestGetAll", "Code %s", ob)
+	for _, user := range ob {
+		beego.Trace("testing", "TestGetAll", "Code %s", user)
+	}
 }
 
-// TestGetOne obtiene un usuario por id
-func TestGetOne(t *testing.T) {
+// TestGetOneHttp obtiene un usuario por id
+func TestGetOneHttp(t *testing.T) {
 	r, _ := http.NewRequest("GET", "/v1/user/888", nil)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
 
-	beego.Trace("testing", "TestGetOne", "Code[%d]\n%s", w.Code, w.Body.String())
+	beego.Trace("testing", "TestGetOneHttp", "Code[%d]\n%s", w.Code, w.Body.String())
 
 	Convey("Subject: Test Station Endpoint\n", t, func() {
 		Convey("Status Code Should Be 200", func() {
@@ -41,4 +43,31 @@ func TestGetOne(t *testing.T) {
 			So(w.Body.Len(), ShouldBeGreaterThan, 0)
 		})
 	})
+}
+
+// TestGetOne obtiene un usuario por id
+func TestGetOne(t *testing.T) {
+	var idUser = 888
+	ob := models.GetUser(idUser)
+	beego.Trace("testing", "TestGetOne", "Code %s", ob)
+
+}
+
+// TestLogin valida login
+func TestLogin(t *testing.T) {
+	var userLog models.Usuarios
+
+	userLog.CLAVE = "123"
+	userLog.CORREO = "fagu@hotm.com"
+	ob := models.ValidateLogin(userLog)
+	beego.Trace("testing", "TestLogin", "Code %s", ob)
+
+}
+
+// TestGetStores obtene las tiendas
+func TestGetStores(t *testing.T) {
+
+	ob := models.GetAllStore()
+	beego.Trace("testing", "TestGetStores", "Code %s", ob)
+
 }
